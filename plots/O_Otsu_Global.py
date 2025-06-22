b055e5fe7209ad4848149e0b9057c0e59128f795
MiguelGzR <miguel.gonzalez_ries@stud.uni-heidelberg.de>
2025-06-22 18:08:14 +0200

    further changes in plots

--- plots/O_Otsu_Global.py
+++ plots/O_Otsu_Global.py
@@ -1,3 +1,4 @@
+# does not work yet
 import os
 from skimage.io import imread
 from glob import glob
@@ -18,7 +19,7 @@ if src_dir not in sys.path:
 output_dir = os.path.join(project_root, "output")
 os.makedirs(output_dir, exist_ok=True)
 
-#
+#   
 def load_n2dh_gowt1_images(base_path="data-git/N2DH-GOWT1"):
     img_dir_N2DH_GOWT1 = os.path.join(base_path, "img")
     gt_dir_N2DH_GOWT1 = os.path.join(base_path, "gt")
@@ -66,49 +67,45 @@ imgs_NIH3T3, gts_NIH3T3, img_paths_NIH3T3, gt_paths_NIH3T3 = load_nih3t3_images(
 
 
 # --------------------------------------------------------------------------
-# Hilfsfunktion zur Dice-Berechnung
+# Importiere deine Otsu-Funktionen und Dice-Score
 from Dice_Score import dice_score
 from src.otsu_global import otsu_threshold
 from src.gray_hist import compute_gray_histogram
-#from otsu_global import apply_global_otsu
-# Hilfsfunktion zur Berechnung der Dice-Werte
-#def berechne_dice_scores(imgs, gts, datensatz_name):
-    # Otsu-Binarisierung
- #   otsu_imgs = []
-  #  for img in imgs:
-   #     otsu_thresh = threshold_otsu(img)
-    #    otsu_imgs.append(img > otsu_thresh)
-    ## GT-Binarisierung
-#    gt_binaries = [gt > 0 for gt in gts]
- #   # Dice-Berechnung
-  #  print(f"\nDice Scores {datensatz_name}:")
-   # for i in range(min(len(otsu_imgs), len(gt_binaries))):
-    #    score = dice_score(otsu_imgs[i], gt_binaries[i])
-     #   print(f"Bild {i}: {score}")
 
-# Hilfsfunktion zur Berechnung der Dice-Werte und Rückgabe der Scores
+# --------------------------------------------------------------
 def berechne_dice_scores(imgs, gts):
-    hist, _ = compute_gray_histogram(imgs)
-    p = hist / hist.sum()
-    otsu_imgs = [img > otsu_threshold(p) for img in imgs]
-    gt_binaries = [gt > 0 for gt in gts]
+    """
+    Berechnet die Dice-Scores zwischen Otsu-binarisierten Bildern und den Ground-Truth-Masken.
+
+    Args:
+        imgs (list[np.ndarray]): Die Eingabebilder.
+        gts (list[np.ndarray]): Die zugehörigen Ground-Truth-Bilder.
+
+    Returns:
+        list[float]: Die berechneten Dice-Scores.
+    """
     scores = []
-    for i in range(min(len(otsu_imgs), len(gt_binaries))):
-        score = dice_score(otsu_imgs[i], gt_binaries[i])
+    for img, gt in zip(imgs, gts):
+        # Histogramm und Wahrscheinlichkeit berechnen
+        hist, _ = compute_gray_histogram(img)
+        p = hist / hist.sum()
+
+        # Otsu-Schwelle berechnen
+        t = otsu_threshold(p)
+
+        # Bild binarisieren
+        otsu_img = img > t
+
+        # GT binarisieren
+        gt_binary = gt > 0
+
+        # Dice-Score berechnen
+        score = dice_score(otsu_img, gt_binary)
         scores.append(score)
     return scores
 
-# --------------------------------------------------------------------------
-# N2DH-GOWT1
-#berechne_dice_scores(imgs_N2DH_GOWT1, gts_N2DH_GOWT1, "N2DH-GOWT1")
-
-# N2DL-HeLa
-#berechne_dice_scores(imgs_N2DL_HeLa, gts_N2DL_HeLa, "N2DL-HeLa")
-
-# NIH3T3
-#berechne_dice_scores(imgs_NIH3T3, gts_NIH3T3, "NIH3T3")
-
-# Berechne die Dice-Scores
+# --------------------------------------------------------------
+# Berechne die Dice-Scores für die drei Datensätze
 dice_gowt1 = berechne_dice_scores(imgs_N2DH_GOWT1, gts_N2DH_GOWT1)
 dice_hela = berechne_dice_scores(imgs_N2DL_HeLa, gts_N2DL_HeLa)
 dice_nih = berechne_dice_scores(imgs_NIH3T3, gts_NIH3T3)
@@ -118,7 +115,7 @@ dice_gowt1 = [float(score) for score in dice_gowt1]
 dice_hela = [float(score) for score in dice_hela]
 dice_nih = [float(score) for score in dice_nih]
 
-# Schön formatiert mit 3 Nachkommastellen ausgeben
-print("GOWT1 Scores:", [f"{score}" for score in dice_gowt1])
-print("HeLa Scores:", [f"{score}" for score in dice_hela])
-print("NIH3T3 Scores:", [f"{score}" for score in dice_nih])
+# Schön formatiert ausgeben
+print("GOWT1 Scores:", [f"{score:.4f}" for score in dice_gowt1])
+print("HeLa Scores:", [f"{score:.4f}" for score in dice_hela])
+print("NIH3T3 Scores:", [f"{score:.4f}" for score in dice_nih])
