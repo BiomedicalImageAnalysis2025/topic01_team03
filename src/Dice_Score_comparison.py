49a0bdf7ad95856e49f53f5f1ee5c40b44303842
MariusMander <mariusmander@gmail.com>
2025-07-06 15:31:34 +0200

    j

--- src/Dice_Score_comparison.py
+++ src/Dice_Score_comparison.py
@@ -206,7 +206,7 @@ def calculate_dice_scores_histeq_global(imgs, gts):
         # binarize groundtruth
         gt_bin = 1 - ((gt / gt.max()) == 0)
 
-        # gamma correction
+        # histogram equalization
         img_eq = histogramequalization(img)
 
         # global otsu thresholding
@@ -236,7 +236,7 @@ def calculate_dice_scores_meanfilter_global(imgs, gts):
         # binarize groundtruth
         gt_bin = 1 - ((gt / gt.max()) == 0)
 
-        # gamma correction
+        # mean filter
         img_filtered = mean_filter(img)
 
         # global otsu thresholding
@@ -266,12 +266,14 @@ def calculate_dice_scores_wienerfilter_global(imgs, gts):
         # binarize groundtruth
         gt_bin = 1 - ((gt / gt.max()) == 0)
 
-        # gamma correction
-        img_filtered = local_wiener_filter(img)
+        # wienfilter-based background estimation and removal
+        background = local_wiener_filter(img)
+        img_wiener = img - background
+
 
         # global otsu thresholding
-        t = otsu_threshold_skimage_like(img_filtered)
-        binary1 = (img_filtered > t)
+        t = otsu_threshold_skimage_like(img_wiener)
+        binary1 = (img_wiener > t)
         # calculate dice score
         score = dice_score(binary1.flatten(), gt_bin.flatten())
         dice_scores.append(score)
