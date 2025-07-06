b1229adc07da6001eabd3865ed9734a8add7cf75
MariusMander <mariusmander@gmail.com>
2025-07-06 15:22:45 +0200

    precoed

--- src/Dice_Score_comparison.py
+++ src/Dice_Score_comparison.py
@@ -247,3 +247,33 @@ def calculate_dice_scores_meanfilter_global(imgs, gts):
         dice_scores.append(score)
 
     return dice_scores
+
+def calculate_dice_scores_wienerfilter_global(imgs, gts):
+    """
+    Process all images and corresponding ground truths to compute a list of Dice scores.
+
+    Args:
+        imgs (list of np.ndarray): Grayscale input images.
+        gts (list of np.ndarray): Corresponding ground-truth masks.
+
+    Returns:
+        dice_scores (list of float): Dice scores for each image-groundtruth pair.
+    """
+    dice_scores = []
+
+    for img, gt in zip(imgs, gts):
+        
+        # binarize groundtruth
+        gt_bin = 1 - ((gt / gt.max()) == 0)
+
+        # gamma correction
+        img_filtered = local_wiener_filter(img)
+
+        # global otsu thresholding
+        t = otsu_threshold_skimage_like(img_filtered)
+        binary1 = (img_filtered > t)
+        # calculate dice score
+        score = dice_score(binary1.flatten(), gt_bin.flatten())
+        dice_scores.append(score)
+
+    return dice_scores
